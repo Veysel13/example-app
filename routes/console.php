b3d8a2e6b0ab2a522253fdb19a4c2b37e4702dc7
Veysel13 <veyselakpinar13@gmail.com>
2024-08-30 18:32:39 +0300

    schecule aktif edildi

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,16 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+
+Schedule::call(function () {
+    Log::info('everyMinute',['everyMinute']);
+})->everyMinute();
+
+Schedule::command('app:test-command')->everyMinute();
