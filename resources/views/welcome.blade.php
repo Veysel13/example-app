e8fcc67370e1cd513aec690ff85fb5c807953946
Veysel Akpınar <36259427+Veysel13@users.noreply.github.com>
2024-09-13 14:10:59 +0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel Veysel 4</title>
+        <title>Laravel Veysel 5</title>
 
 
         <!-- Fonts -->
