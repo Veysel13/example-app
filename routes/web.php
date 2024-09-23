338bf7b6da0dc3b4b4426354104004c78fc14f07
Veysel13 <veyselakpinar13@gmail.com>
2024-09-23 21:04:09 +0300

    added rabbit

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    \App\Jobs\ExampleJob::dispatch()->onQueue('first');
+    //\App\Jobs\ExampleJob::dispatch()->onConnection('database');
+
     return view('welcome');
 });
